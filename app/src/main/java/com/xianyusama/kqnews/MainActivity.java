520bafccb52f5f843ef8c64d4e280753e882dc67
xkq <xianyusama@gmail.com>
2017-03-28 22:13:06 +0800

    获取数据

--- app/src/main/java/com/xianyusama/kqnews/MainActivity.java
+++ app/src/main/java/com/xianyusama/kqnews/MainActivity.java
@@ -7,37 +7,31 @@ import com.socks.library.KLog;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.Callback;
 
+import java.io.IOException;
+
 import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+String url="http://v.juhe.cn/toutiao/index?type=top&key=90139c3055bd2345d52ce2c76985f085";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpUtils
-                .post()
-                .url("http://v.juhe.cn/toutiao/index")
-                .addParams("type", "top")
-                .addParams("password", "123")
-                .build()
-                .execute(new Callback() {
-                    @Override
-                    public Object parseNetworkResponse(Response response, int id) throws Exception {
-                        return null;
-                    }
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
+            }
 
-                    }
-
-                    @Override
-                    public void onResponse(Object response, int id) {
-                        String s = response.toString();
-                        KLog.d(s);
-                    }
-                });
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                KLog.d(response.body().string());
+            }
+        });
     }
 }
